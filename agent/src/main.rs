bc987af61c30ea4dd27002f8ce10152e3b716f09
zzwtsy <zzwtsy@yumdeb.top>
2024-09-01 22:31:40 +0800

    feat(agent): 重构Agent程序入口

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use command::Command;
+use server_monitor_info_upload::ServerMonitorAgent;
+
+mod command;
+mod dto;
+mod fetch_ip;
+mod server_monitor_info_upload;
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    let command = Command::parse();
+    // TODO: 待进一步完善
+    ServerMonitorAgent::new(command.url, command.port, 1)
+        .await
+        .report_server_monitor()
+        .await;
 }
